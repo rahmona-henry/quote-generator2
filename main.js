b84ea89496775f76ff0bb88a0601538c0203c1dd
rahmona-henry <someone@devacademy.co.nz>
2016-02-26 18:34:05 +1300

    add quotes array

---
+++ main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+  var quoteBank = {
+    'quotes': [
+      {
+        'quote': 'Hello from the other side',
+        'author': 'Adele'
+      },
+
+      {
+        'quote': 'Hello from the other side',
+        'author': 'Adele'
+      },
+
+      {
+        'quote': 'Hello from the other side',
+        'author': 'Adele'
+      }
+    ]
+
+  }
+
+ console.log('quotes');
+
+
+
+
+
+
+
+
+
+
+});
